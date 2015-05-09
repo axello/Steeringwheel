72e72ef0a30e5a547fce394fcc773ea5f7ef1238
Axel Roest <axel@phlux.us>
2015-05-09 17:45:12 +0200

    Added Arduino code to repository

---
+++ Arduino/Code/code 5/accelerometer_vibrate_neopixels5/accelerometer_vibrate_neopixels5.ino
@@ -0,0 +1,247 @@
+#include <Wire.h> 
+#include <ADXL345.h> 
+#include <Adafruit_NeoPixel.h>
+
+#define PIN 6 
+
+const int MAXVIB=180; // 180 is 3.6V if Vin is 5V (Arduino Uno)
+
+const int potPin = A0;  // Analog input pin that the potentiometer is attached to
+const int motorPin = 3; // Analog output pin that the vibration motor is attached to
+//int ledPin = 6;      // PWM pin that the LED is on.  n.b. PWM 0 is on digital pin 9
+int outputValue = 0;        // value output to the PWM (analog out)
+boolean ledOn = false;
+boolean motorOn = false;
+int shakeTimer = 0;
+int lightTimer = 0;
+ 
+ADXL345 adxl; //variable adxl is an instance of the ADXL345 library 
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(60, PIN, NEO_GRB + NEO_KHZ800);
+ 
+void setup(){ 
+
+Serial.begin(9600);
+//  pinMode(ledPin, OUTPUT);
+  pinMode(motorPin, OUTPUT);
+  pinMode(potPin, INPUT);
+ 
+ adxl.powerOn(); 
+ strip.begin();
+ strip.show();  
+} 
+
+void loop(){ 
+    
+ //clearStrip (); 
+ int x,y,z; 
+ adxl.readAccel(&x, &y, &z); //read the accelerometer values and store them in variables x,y,z 
+
+   uint16_t acceleration = abs(x>>1) + abs(y>>1);
+ 
+   // read the analog in value:
+  // int  sensitivity = analogRead(potPin);   
+  uint16_t upper_sensitivity = analogRead(potPin) + 4;
+  uint16_t lower_sensitivity = upper_sensitivity - 8; 
+ 
+  uint16_t led5 = 0;
+  uint16_t led4 = (lower_sensitivity / 5) * 1;
+  uint16_t led3 = (lower_sensitivity / 5) * 2;
+  uint16_t led2 = (lower_sensitivity / 5) * 3;
+  uint16_t led1 = (lower_sensitivity / 5) * 4;
+  
+  if (acceleration > upper_sensitivity) {
+    clearStrip ();
+    // map it to the range of the analog out:
+    outputValue = map(acceleration, upper_sensitivity, 512, 0, MAXVIB);  
+    // turn motor on accordingly  
+      analogWrite(motorPin, outputValue);
+    motorOn = true;
+} else 
+
+if (acceleration > lower_sensitivity) {
+   // all leds off 
+      analogWrite(motorPin, 0);
+      motorOn=false;
+    clearStrip ();
+         
+  } else if (acceleration > led1) {
+      analogWrite(motorPin, 0);
+      motorOn=false;
+    // one led on
+       for(int i = 12; i<18; i++){
+        strip.setPixelColor(i, strip.Color(0, 50, 0)); 
+        strip.show();
+      }       
+  } else if (acceleration > led2) {
+      analogWrite(motorPin, 0);
+      motorOn=false;
+   // two led on
+          for(int i = 9; i<21; i++){
+          strip.setPixelColor(i, strip.Color(0, 50, 0)); 
+          strip.show();
+        }       
+  } else if (acceleration > led3) {
+      analogWrite(motorPin, 0);
+      motorOn=false;
+   // three leds on
+        for( int i = 6; i<24; i++){
+          strip.setPixelColor(i, strip.Color(0, 50, 0)); 
+          strip.show();
+        }        
+  } else if (acceleration > led4) {
+      analogWrite(motorPin, 0);
+      motorOn=false;
+   // four leds on
+        for(int i = 3; i<27; i++){
+          strip.setPixelColor(i, strip.Color(0, 50, 0)); 
+          strip.show();
+        }   
+  } else if (acceleration > led5) {
+      analogWrite(motorPin, 0);
+      motorOn=false;
+   // five leds on
+        for(int i = 0; i<30; i++){
+          strip.setPixelColor(i, strip.Color(0, 50, 0)); 
+          strip.show();
+        }
+  } else 
+   
+{
+    // turn motor off
+  /*    motorOn = false;
+      analogWrite(motorPin, 0);
+      */
+  }
+ 
+ 
+ /////////////////////////////////////////////////////////////////////////
+ 
+ 
+/* 
+ 
+  if (acceleration<=15) {
+    shakeTimer--;
+    if( shakeTimer == 0){
+      motorOn = false;
+      analogWrite(motorPin, 0);
+    }
+    lightTimer--;
+    if( lightTimer ==0){
+      clearStrip ();
+    }   
+  }
+  else if (acceleration < (sensitivity-15)/5)
+  {
+    lightTimer = 20;
+    shakeTimer--;
+    if( shakeTimer == 0){
+      motorOn = false;
+      analogWrite(motorPin, 0);
+    }
+    if (motorOn == false){
+      for(  int i = 12; i<18; i++){
+        strip.setPixelColor(i, strip.Color(0, 50, 0)); 
+        strip.show();
+      }   
+    }    
+  }
+  
+    else if (acceleration < ((sensitivity-15)/5)*2)
+    {
+      lightTimer = 20;
+      shakeTimer--;
+      if( shakeTimer == 0){
+        motorOn = false;
+        analogWrite(motorPin, 0);
+      }
+      if (motorOn == false){
+        for( int i = 9; i<21; i++){
+          strip.setPixelColor(i, strip.Color(0, 50, 0)); 
+          strip.show();
+        }   
+      }   
+    } 
+    else if (acceleration < ((sensitivity-15)/5)*3)
+    {
+      lightTimer = 20;
+      shakeTimer--;
+      if( shakeTimer == 0){
+        motorOn = false;
+        analogWrite(motorPin, 0);
+      }
+      if (motorOn == false){
+        for( int i = 6; i<24; i++){
+          strip.setPixelColor(i, strip.Color(0, 50, 0)); 
+          strip.show();
+        }   
+      }  
+    }  
+  
+    else if (acceleration < ((sensitivity-15)/5)*4)
+    {
+      lightTimer = 20;
+      shakeTimer--;
+      if( shakeTimer == 0){
+        motorOn = false;
+        analogWrite(motorPin, 0);
+      }
+      if (motorOn == false){
+        for( int i = 3; i<27; i++){
+          strip.setPixelColor(i, strip.Color(0, 50, 0)); 
+          strip.show();
+        }   
+      }
+    }  
+    else if (acceleration < ((sensitivity-15)/5)*5)
+    {
+      lightTimer = 20;
+      shakeTimer--;
+      if( shakeTimer == 0){
+        motorOn = false;
+        analogWrite(motorPin, 0);
+      }
+    
+      if (motorOn == false){
+        for( int i = 0; i<30; i++){
+          strip.setPixelColor(i, strip.Color(0, 50, 0)); 
+          strip.show();
+        }
+      } 
+    }  
+   else if (acceleration > (sensitivity)) {
+    // change the analog out value:
+      clearStrip ();
+      shakeTimer = 20;
+      analogWrite(motorPin, outputValue);
+      motorOn = true;
+    } 
+  
+  else {
+   return;
+  }
+    
+  */
+  
+ // Output x,y,z values - Commented out 
+ Serial.print("acc:"); 
+ Serial.print(acceleration);
+ Serial.print('\t'); 
+ Serial.print("sens:");
+ Serial.print(upper_sensitivity);
+ Serial.print('\t'); 
+ Serial.print(lower_sensitivity);
+ Serial.print('\t'); 
+ Serial.print(x); 
+ Serial.print('\t'); 
+ Serial.println(y); 
+ //Serial.print('\t'); 
+ //Serial.println(z); 
+ delay(50); 
+
+} 
+
+void clearStrip() {
+  for( int i = 0; i<30; i++){
+    strip.setPixelColor(i, 0x000000); strip.show();
+  }
+}
---
+++ Arduino/SteeringWheel/SteeringWheel.ino
@@ -0,0 +1,349 @@
+// #include <RBL_services.h>
+
+#include "Adafruit_NeoPixel.h"
+#include <SPI.h>
+#include <boards.h>
+#include <RBL_nRF8001.h>
+#include <services.h>
+
+// pin modes
+//#define INPUT                 0x00 // defined in wiring.h
+//#define OUTPUT                0x01 // defined in wiring.h
+#define ANALOG                  0x02 // analog pin in analogInput mode
+#define PWM                     0x03 // digital pin in PWM output mode
+#define SERVO                   0x04 // digital pin in Servo output mode
+
+
+// Parameter 1 = number of pixels in strip
+// Parameter 2 = pin number (most are valid)
+// Parameter 3 = pixel type flags, add together as needed:
+//   NEO_RGB     Pixels are wired for RGB bitstream
+//   NEO_GRB     Pixels are wired for GRB bitstream
+//   NEO_KHZ400  400 KHz bitstream (e.g. FLORA pixels)
+//   NEO_KHZ800  800 KHz bitstream (e.g. High Density LED strip)
+// Adafruit_NeoPixel strip = Adafruit_NeoPixel(1, 7, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(30, 6, NEO_GRB + NEO_KHZ800);
+
+int R = 0;
+int G = 0;
+int B = 0;
+
+static byte buf_len = 0;
+
+void setup() 
+{
+    Serial.begin(115200);
+  Serial.println("BLE SteeringWheel");
+
+  strip.begin();
+  strip.show(); // Initialize all pixels to 'off'
+
+  ble_begin();
+}
+
+void loop() 
+{
+  while(ble_available()) {
+    byte cmd;
+    cmd = ble_read();
+    
+    // temp
+//        switch (cmd)
+//    {
+//      case 'R':      // RGB values
+//      {
+//        byte pin = ble_read();
+//        R = ble_read();
+//        G = ble_read();
+//        B = ble_read();
+//        
+//        uint32_t color = strip.Color(R, G, B);
+//        reportRGBValues(color);
+//        strip.setPixelColor(pin, R,G,B);
+//        strip.show();
+//
+//      } break;
+//      case 'V': // query protocol version
+//        {
+//          byte buf[] = {'V', 0x00, 0x00, 0x01};
+//          ble_write_string(buf, 4);
+//        }
+//        break;
+//      
+//      case 'C': // query board total pin count
+//        {
+//          byte buf[2];
+//          buf[0] = 'C';
+//          buf[1] = 'C'; 
+//          ble_write_string(buf, 2);
+//        }        
+//        break;
+//      
+//      case 'M': // query pin mode
+//        {  
+//          byte pin = ble_read();
+//          byte buf[] = {'M', pin, 1}; // report pin mode
+//          ble_write_string(buf, 3);
+//        }  
+//        break;
+//      
+//        case 'S': // set pin mode
+//        {
+//          byte pin = ble_read();
+//          byte mode = ble_read();
+//          setPin(pin,mode);
+//          reportPinDigitalData(pin);
+//
+//        }
+//        break;
+//
+//      case 'G': // query pin data
+//        {
+//          byte pin = ble_read();
+//          reportPinDigitalData(pin);
+//        }
+//        break;
+//        
+//      case 'T': // set pin digital state
+//        {
+//          byte pin = ble_read();
+//          byte state = ble_read();
+//          
+//          digitalWrite(pin, state);
+//          reportPinDigitalData(pin);
+//        }
+//        break;
+//      
+//      case 'N': // set PWM
+//        {
+//          byte pin = ble_read();
+//          byte value = ble_read();
+//          
+//          analogWrite(pin, value);
+//          reportPinPWMData(pin);
+//        }
+//        break;
+//      
+//      case 'O': // set Servo
+//        {
+//          byte pin = ble_read();
+//          byte value = ble_read();
+//          reportPinServoData(pin);
+//        }
+//        break;
+//      
+//      case 'A': // query all pin status
+//          reportPinCapability(0);
+//         reportPinDigitalData(0);
+//        
+//        {
+//          uint8_t str[] = "ABC";
+//          sendCustomData(str, 3);
+//        }
+//       
+//        break;
+//          
+//      case 'P': // query pin capability
+//        {
+//          byte pin = ble_read();
+//          reportPinCapability(pin);
+//        }
+//        break;
+//        
+//      case 'Z':
+//        {
+//          byte len = ble_read();
+//          byte buf[len];
+//          for (int i=0;i<len;i++)
+//            buf[i] = ble_read();
+//          Serial.println("->");
+//          Serial.print("Received: ");
+//          Serial.print(len);
+//          Serial.println(" byte(s)");
+//          Serial.print(" Hex: ");
+//          for (int i=0;i<len;i++)
+//            Serial.print(buf[i], HEX);
+//          Serial.println();
+//        }
+//    }
+
+    /// end temp
+    // Parse data here
+    switch (cmd)
+    {
+        case 'S': // set pin mode
+        {
+          byte pin = ble_read();
+          byte mode = ble_read();
+          setPin(pin,mode);
+        }
+        break;
+        case 'T': // set pin digital state
+        {
+          byte pin = ble_read();
+          byte state = ble_read();
+          
+          digitalWrite(pin, state);
+        }
+        break;
+        case 'N': // set PWM
+        {
+          byte pin = ble_read();
+          byte value = ble_read();
+          analogWrite(pin, value);
+        }
+        break;
+      case 'R':      // RGB values
+      {
+        byte pin = ble_read();
+        R = ble_read();
+        G = ble_read();
+        B = ble_read();
+        
+        uint32_t color = strip.Color(R, G, B);
+   //     reportRGBValues(color);
+       Serial.print(pin);
+       Serial.print(" ");
+        strip.setPixelColor(pin, R,G,B);
+        strip.show();
+
+      } break;
+    }
+   
+  }
+  ble_do_events();
+}
+
+// Fill the dots one after the other with a color
+void colorWipe(uint32_t c) 
+{
+  for(uint16_t i=0; i<strip.numPixels(); i++) 
+  {
+      strip.setPixelColor(i, c);
+  }
+  strip.show();
+
+}
+
+void setPin(byte pin, byte mode)
+{
+  /* ToDo: check the mode is in its capability or not */
+  /* assume always ok */
+    pinMode(pin, mode);
+  
+    if (mode == OUTPUT)
+    {
+      digitalWrite(pin, LOW);
+    }
+    else if (mode == INPUT)
+    {
+      digitalWrite(pin, HIGH);
+    }
+    else if (mode == ANALOG)
+    {
+          pinMode(pin, LOW);
+    }
+    else if (mode == PWM)
+    {
+        pinMode(pin, OUTPUT);
+        analogWrite(pin, 0);
+    }
+}
+
+void reportRGBValues(uint32_t color)
+{
+//  if (IS_PIN_SERVO(pin))
+//    servos[PIN_TO_SERVO(pin)].write(value);
+//  pin_servo[pin] = value;
+
+  byte blue = color & 0xFF;
+  byte green = (color & 0xFF00) >> 8;
+  byte red = (color & 0xFF0000) >> 16;
+  
+  byte buf[] = {'R', 'G', 'B', red, green, blue};
+  Serial.print("RGB: ");
+  Serial.print(red);
+  Serial.print(green);
+  Serial.println(blue);
+}
+
+
+void ble_write_string(byte *bytes, uint8_t len)
+{
+  if (buf_len + len > 20)
+  {
+    for (int j = 0; j < 15000; j++)
+      ble_do_events();
+    
+    buf_len = 0;
+  }
+  
+  for (int j = 0; j < len; j++)
+  {
+    ble_write(bytes[j]);
+    buf_len++;
+  }
+    
+  if (buf_len == 20)
+  {
+    for (int j = 0; j < 15000; j++)
+      ble_do_events();
+    
+    buf_len = 0;
+  }  
+}
+
+
+void sendCustomData(uint8_t *buf, uint8_t len)
+{
+  uint8_t data[20] = "Z";
+  memcpy(&data[1], buf, len);
+  ble_write_string(data, len+1);
+}
+
+void reportPinCapability(byte pin)
+{
+  byte buf[] = {'P', pin, 0x00};
+  byte pin_cap = 0;
+                    
+  buf[2] = pin_cap;
+  ble_write_string(buf, 3);
+}
+
+
+void reportPinDigitalData(byte pin)
+{
+  byte state = digitalRead(pin);
+  byte buf[] = {'G', pin, 1, 1};         
+  ble_write_string(buf, 4);
+}
+
+void reportPinPWMData(byte pin)
+{
+  byte buf[] = {'G', pin, 1, 1};         
+  ble_write_string(buf, 4);
+}
+
+
+
+void reportPinServoData(byte pin)
+{
+//  if (IS_PIN_SERVO(pin))
+//    servos[PIN_TO_SERVO(pin)].write(value);
+//  pin_servo[pin] = value;
+  
+  byte buf[] = {'G', pin, 1, 1};         
+  ble_write_string(buf, 4);
+}
+
+byte reportPinAnalogData()
+{
+  if (!ble_connected())
+    return 0;
+    
+  static byte pin = 0;
+  byte report = 0;
+     
+  return report;
+}
+
